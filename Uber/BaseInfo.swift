56834b349a7ae613c5e7a77b1098c2cb8dffe6f5
ZeroneCoding <1043481495@qq.com>
2016-06-13 08:42:24 +0800

    Uber Code

---
+++ Uber/BaseInfo.swift
@@ -0,0 +1,13 @@
+//
+//  BaseInfo.swift
+//  Uber
+//
+//  Created by 李银 on 16/6/13.
+//  Copyright © 2016年 liyin. All rights reserved.
+//
+
+import UIKit
+
+class BaseInfo: NSObject {
+
+}
---
+++ Uber/FxGetCars.swift
@@ -0,0 +1,13 @@
+//
+//  FxGetCars.swift
+//  Uber
+//
+//  Created by 李银 on 16/6/13.
+//  Copyright © 2016年 liyin. All rights reserved.
+//
+
+import UIKit
+
+class FxGetCars: FxOperation {
+
+}
